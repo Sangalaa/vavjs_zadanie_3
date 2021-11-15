1232336339d3673c236746804904a405d93f9f32
Adam Bublavý <bublavy.adam@gmail.com>
2021-11-15 22:31:20 +0100

    Added database and .env setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import "dotenv-safe/config.js"
+import { Sequelize } from 'sequelize'
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
 
-const PORT = 8080
+// database setup
+const seq = new Sequelize(process.env.DATABASE_URL)
 
-app.listen(PORT, () => {
-    console.log(`Server is running at port: ${PORT}`)
+app.listen(parseInt(process.env.PORT), () => {
+    console.log(`Server is running at port: ${process.env.PORT}`)
 })
