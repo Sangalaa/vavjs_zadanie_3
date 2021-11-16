b2d440f30b9eda2824aa69447e8660484b7e31e0
Adam Bublavý <bublavy.adam@gmail.com>
2021-11-17 00:46:07 +0100

    Added: Routes boilerplate

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,17 @@
-function App() {
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import * as ROUTES from './constants/routes'
+
+const Products = lazy(() => import('./pages/products'))
+
+export default function App() {
   return (
-    <div className="App">
-      <h2 className="text-4xl text-bold">Ahoj</h2>
-      Working
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path={ ROUTES.PRODUCTS } element={<Products />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
-
-export default App;
---
+++ Frontend/src/constants/routes.js
@@ -0,0 +1,2 @@
+export const PRODUCTS = '/'
+export const ADMIN_PAGE = '/admin'
